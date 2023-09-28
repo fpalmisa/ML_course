952b975c68a264c7318e3327dc4103479c19e7d3
fpalmisa <101518342+fpalmisa@users.noreply.github.com>
2023-09-28 12:24:35 +0200

    Week2_Exo1and2

--- labs/ex02/template/costs.py
+++ labs/ex02/template/costs.py
@@ -18,5 +18,12 @@ def compute_loss(y, tx, w):
     # ***************************************************
     # INSERT YOUR CODE HERE
     # TODO: compute loss by MSE
+
+    loss = 0
+    for n in range(len(y)):
+        MSE = y[n] - w[0] - w[1]*tx[n,1]
+        loss = loss + np.power(MSE,2)
+
+    loss = loss/(len(y))
+    return loss
     # ***************************************************
-    raise NotImplementedError
